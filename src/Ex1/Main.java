f2878300154aa070099d709a2881d35c41e6c228
Hammers <gianluca.hammers@gmail.com>
2026-01-12 14:33:37 +0100

    Commit prova

--- src/Ex1/Main.java
+++ src/Ex1/Main.java
@@ -1,15 +1,43 @@
+package Ex1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] arr = new int[5];
+        for (int i = 0 ; i < arr.length; i++){
+            int random = (int) Math.ceil(Math.random() * 10);
+            arr[i] = random;
+        }
+        Scanner sc = new Scanner(System.in);
+        int numero ;
+        int posizione;
+
+        while (true) {
+        System.out.println(Arrays.toString(arr));
+        System.out.print("Inserisci il numero che vuoi posizionare (inserisci 0 per terminare): ");
+        try {
+            numero = Integer.parseInt(sc.nextLine());
+        }catch (NumberFormatException e) {
+            System.out.println("Errore: devi inserire un numero intero!");
+            continue;
+        };
+            if (numero == 0) {
+                System.out.println("Programma terminato.");
+                break;
+            }
+            System.out.print("In che posizione dell'array vuoi inserire il numero? (da 0 a 4): ");
+        try {
+            posizione = Integer.parseInt(sc.nextLine());
+            arr[posizione] = numero;
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            System.out.println("Errore: Posizione non valido oppure non e' un numero!");
+        }
         }
     }
 }
+
