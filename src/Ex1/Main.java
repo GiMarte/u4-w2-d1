a61892c6f8cfab5423eb38a6f1be15ccfdf23793
Hammers <gianluca.hammers@gmail.com>
2026-01-22 17:00:09 +0100

    Second commit

--- src/Ex1/Main.java
+++ src/Ex1/Main.java
@@ -9,35 +9,37 @@ public class Main {
     public static void main(String[] args) {
 
         int[] arr = new int[5];
-        for (int i = 0 ; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             int random = (int) Math.ceil(Math.random() * 10);
             arr[i] = random;
         }
         Scanner sc = new Scanner(System.in);
-        int numero ;
+        int numero;
         int posizione;
 
         while (true) {
-        System.out.println(Arrays.toString(arr));
-        System.out.print("Inserisci il numero che vuoi posizionare (inserisci 0 per terminare): ");
-        try {
-            numero = Integer.parseInt(sc.nextLine());
-        }catch (NumberFormatException e) {
-            System.out.println("Errore: devi inserire un numero intero!");
-            continue;
-        };
+            System.out.println(Arrays.toString(arr));
+            System.out.print("Inserisci il numero che vuoi posizionare (inserisci 0 per terminare): ");
+            try {
+                numero = Integer.parseInt(sc.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Errore: devi inserire un numero intero!");
+                continue;
+            }
+            ;
             if (numero == 0) {
                 System.out.println("Programma terminato.");
                 break;
             }
             System.out.print("In che posizione dell'array vuoi inserire il numero? (da 0 a 4): ");
-        try {
-            posizione = Integer.parseInt(sc.nextLine());
-            arr[posizione] = numero;
-        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
-            System.out.println("Errore: Posizione non valido oppure non e' un numero!");
-        }
+            try {
+                posizione = Integer.parseInt(sc.nextLine());
+                arr[posizione] = numero;
+            } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+                System.out.println("Errore: Posizione non valido oppure non e' un numero!");
+            }
         }
     }
 }
+
 
